53d768dec9708b1f8245a846d8153293cef88fe5
AhmedSherif2002 <ahmedsherif602@gmail.com>
2024-10-09 22:50:37 +0300

    Deployment

--- backend/dist/controller.js
+++ backend/dist/controller.js
@@ -1,11 +1,54 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const a = 10000;
-console.log(a);
-const deploy = (repo, app) => {
-    console.log(repo, app);
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
 };
-deploy("r", "a");
+Object.defineProperty(exports, "__esModule", { value: true });
+const models_1 = require("./models");
+const { exec } = require("child_process");
+const deploy = (repo, app) => __awaiter(void 0, void 0, void 0, function* () {
+    var _a;
+    try {
+        const found = yield models_1.apps.find({ name: app });
+        if (found.length !== 0) {
+            console.log(found);
+            throw ("Application name is already used");
+        }
+        const maxPort = (_a = (yield models_1.apps.find({}).sort({ port: -1 }))[0]) === null || _a === void 0 ? void 0 : _a.port;
+        const port = maxPort ? maxPort + 1 : 3500;
+        console.log(maxPort, port);
+        cloneRepo(repo, app, port);
+    }
+    catch (err) {
+        console.log(err);
+    }
+});
+const cloneRepo = (repo, appName, port) => {
+    const cloneRepo = exec(`git clone ${repo} ~/static-apps/${appName}`);
+    cloneRepo.stdout.on("data", (data) => {
+        console.log(data);
+    });
+    cloneRepo.stderr.on("data", (error) => {
+        console.error(error);
+    });
+    cloneRepo.on("close", (code) => {
+        console.log(`Process exited with code ${code}`);
+        if (code === 0) {
+            console.log("done");
+            update(repo, appName, port);
+        }
+    });
+};
+const update = (repo, appName, port) => __awaiter(void 0, void 0, void 0, function* () {
+    const inserted = yield models_1.apps.create({ appName, repo, port });
+    console.log(inserted);
+});
+deploy("https://github.com/AhmedSherif2002/statictest.git", "app2");
 /*
     - Make a directory for the application.
     - Deploy the application in that directory.
--- backend/src/controller.ts
+++ backend/src/controller.ts
@@ -1,13 +1,51 @@
-const a:any = 10000;
-console.log(a)
+import { apps, ports } from "./models";
+const { exec } = require("child_process");
 
-const deploy = (repo:string, app:string)=>{
-    console.log(repo, app);
+const deploy = async (repo:string, app:string)=>{
+    try{
+        const found = await apps.find({name: app});
+        if(found.length !== 0){
+            console.log(found);
+            throw("Application name is already used");
+        }
+        const maxPort = (await apps.find({}).sort({ port: -1 }))[0]?.port;
+        const port = maxPort?maxPort+1:3500;
+        console.log(maxPort,port);
+        cloneRepo(repo, app, port);
+    }catch(err){
+        console.log(err);
+    }
 } 
 
-deploy("r","a");
+const cloneRepo = (repo:string, appName:string,port:Number)=>{
+    const cloneRepo = exec(`git clone ${repo} ~/static-apps/${appName}`);
+    cloneRepo.stdout.on("data", (data:any)=>{
+        console.log(data);
+    })
+    cloneRepo.stderr.on("data", (error:any)=>{
+        console.error(error);
+    })
+    cloneRepo.on("close", (code:any)=>{
+        console.log(`Process exited with code ${code}`);
+        if(code === 0){
+            console.log("done")
+            update(repo,appName,port);
+        }
+    })
+}
 
-export {};
+
+const update = async (repo:String, appName:String, port:Number) =>{
+    const inserted = await apps.create({ appName, repo, port});
+    console.log(inserted);
+
+}
+
+
+deploy("https://github.com/AhmedSherif2002/statictest.git","app2");
+// deploy("https://github.com/AhmedSherif2002/statictest.git","app");
+
+export { };
 
 
 /*  
