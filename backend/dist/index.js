27bbbaac4bd7cb5c357e9919f636c180c8e1488d
AhmedSherif2002 <ahmedsherif602@gmail.com>
2024-10-13 14:51:00 +0300

    Backend was completed

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,4 +1,29 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const a = 10;
-console.log(a);
+const controller_1 = require("./controller");
+const express = require("express");
+const app = express();
+// const bodyParser = require("body-parser")
+app.use(express.json());
+app.listen(3000, () => {
+    console.log("Server is Running on port", 3000);
+});
+app.get("/", (req, res) => {
+    console.log(req.body);
+    const repo = req.body.repo_url;
+    const appname = req.body.appname;
+    (0, controller_1.deploy)(repo, appname, (err, port) => {
+        if (err) {
+            console.log(err);
+            return res.json({
+                success: false,
+                message: "Error"
+            });
+        }
+        res.json({
+            success: true,
+            message: "App was deployed",
+            port: port
+        });
+    });
+});
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,31 @@
-import { appendFile } from "fs";
+import { deploy } from "./controller";
 
-const a:any = 10;
-console.log(a);
+const express = require("express");
+const app = express();
+// const bodyParser = require("body-parser")
+
+app.use(express.json());
+
+app.listen(3000, ()=>{
+    console.log("Server is Running on port", 3000);
+})
+
+app.get("/",(req:any, res:any)=>{
+    console.log(req.body);
+    const repo = req.body.repo_url;
+    const appname = req.body.appname;
+    deploy(repo, appname, (err:any, port:Number)=>{
+        if(err){
+            console.log(err);
+            return res.json({
+                success: false,
+                message: "Error"
+            });
+        }
+        res.json({
+            success: true,
+            message: "App was deployed",
+            port: port 
+        });
+    })
+})
